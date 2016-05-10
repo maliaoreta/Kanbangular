54daffdee1ff6e540c8e76c5e077f6083df18cc4
Taylor Kennedy <taykenned@gmail.com>
2016-05-10 12:41:19 -1000

    fixed missing bracket in models index

--- models/index.js
+++ models/index.js
@@ -10,7 +10,7 @@ var db        = {};
 
 //if (config.use_env_variable) {
 //  var sequelize = new Sequelize(process.env[config.use_env_variable]);
-if(process.env.DATABASE_URL)
+if(process.env.DATABASE_URL) {
   var sequelize = new Sequelize(process.env.DATABASE_URL);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
