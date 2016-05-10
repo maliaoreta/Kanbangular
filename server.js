35a90891e08e00a03a37177a37666af6ac07c63c
Taylor Kennedy <taykenned@gmail.com>
2016-05-10 11:35:00 -1000

    adds environment variable for secret key

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
-  secret: 'skgja;djg;asjg;adjg;sjdgk',
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: false
 }));
